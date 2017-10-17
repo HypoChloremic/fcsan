f630fc2429d9763f0a8fb8f8f6b4fac98288c907
HypoChloremic <ali.rassolie@gmail.com>
2017-10-17 09:09:38 +0200

    edit

--- source/analyze.py
+++ source/analyze.py
@@ -19,6 +19,7 @@ from sklearn.cluster import KMeans
 from pandas import DataFrame as df
 from pandas import concat, cut
 from math import log
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import use
@@ -36,9 +37,8 @@ class Analyze:
 		print("[GLOBAL] Opening config")
 		with open(config, "r") as file:
 			self.config = {i.split(": ")[0]: i.split(": ")[1].replace("\n", "") for i in file.readlines()}
-			print(self.config)
 			self.path   = self.config["PARENT"] 
-			print(self.path)
+
 
 		print(f"[GLOBAL] The parent-path: {self.path}")
 		
@@ -63,16 +63,19 @@ class Analyze:
   ########################
   ### Analysis methods ###
   ########################
-	def kmeans(self, dataset=None, nclusters=2, logx=False, logy=False, dimensions=2, transpose=False, channels=None, **kwargs):
+	def kmeans(self, dataset=None, nclusters=2, logx=False, logy=False, limit_dataset=None, transpose=False, channels=None, **kwargs):
 
 		print("[KMeans] Running KMeans clustering")
 		if dataset is None: dataset = self.dataset
+		if limit_dataset: pr_dataset = df(dataset, index=dataset.index, columns=limit_dataset)
+		elif not limit_dataset: pr_dataset = df(dataset, index=dataset.index)
+
 
 		# Necessary for seaborn, providing the number of 
 		# clusters to color
-		predict   = KMeans(n_clusters=nclusters, **kwargs).fit(dataset)
-		predicted = df(predict.predict(self.dataset), columns=["mapping"])
-		dataset = concat([dataset, predicted], axis=1)
+		predict   = KMeans(n_clusters=nclusters, **kwargs).fit(pr_dataset)
+		predicted = df(predict.predict(pr_dataset), columns=["mapping"])
+		dataset   = concat([dataset, predicted], axis=1)
 
 		if channels and not transpose: x,y=channels[0],channels[1]
 		elif channels and transpose:   x,y=channels[1],channels[0]
@@ -97,7 +100,7 @@ class Analyze:
 		if i > 0: return log(i)
 		else: return None
 
-	def plot(self, dataset=None, xfunc=None, yfunc=None, transpose=False, save=False, **kwargs):
+	def plot(self, dataset=None, xfunc=None, yfunc=None, transpose=False, save=False, threeD=False, **kwargs):
 		# We are using the plot method associated with the dataframe
 		# Note that that transposing the 
 		if not dataset: dataset = self.dataset 
@@ -109,6 +112,26 @@ class Analyze:
 		if yfunc: dataset[kwargs["y"]] = dataset[kwargs["y"]].apply(func=lambda y: yfunc(y))
 		self.dataset.plot(**kwargs)
 		if not save: plt.show()
+
+	def plot_3d(self, dataset=None, xfunc=None, yfunc=None, zfunc=None, save=False, threeD=False, kind="scatter", transpose=False, **kwargs):
+		# We are using the plot method associated with the dataframe
+		# Note that that transposing the 
+		if not dataset: dataset = self.dataset 
+		if transpose: 
+			temp = kwargs["x"]
+			kwargs["x"] = kwargs["y"]
+			kwargs["y"] = temp
+		if xfunc: dataset[kwargs["x"]] = dataset[kwargs["x"]].apply(func=lambda x: xfunc(x))
+		if yfunc: dataset[kwargs["y"]] = dataset[kwargs["y"]].apply(func=lambda y: yfunc(y))
+		if yfunc: dataset[kwargs["z"]] = dataset[kwargs["z"]].apply(func=lambda z: yfunc(z))
+
+		threedee = plt.figure().gca(projection="3d")
+		if kind=="scatter": threedee.scatter(dataset[kwargs["x"]],dataset[kwargs["y"]],dataset[kwargs["z"]])
+		threedee.set_xlabel(kwargs["x"])
+		threedee.set_ylabel(kwargs["y"])
+		threedee.set_zlabel(kwargs["z"])
+		if not save: plt.show()
+
 
 	def freq(self, column, dataset=None, scope = 64, func=None, *args, **kwargs):
 		print("[FREQ] Running frequency method")
@@ -122,7 +145,7 @@ class Analyze:
 
 		return frequency
 
-	def saveplots(self, func=None, folder=None, rdata=False, delimiter="\\", description=None, *args, **kwargs):
+	def saveplots(self, func=None, folder=None, rdata=False, delimiter="\\", description=None, log_overwrite=True, logfile="log.txt", *args, **kwargs):
 		if not folder: folder=self.config["OUTPUT"]
 
 		use("Agg")
@@ -139,35 +162,79 @@ class Analyze:
 			elif func and rdata:
 				data = func(*args, **kwargs)
 				data.plot()
+
 			plt.savefig(name)
-			plt.clf()
+			plt.close()
+			if log_overwrite and pos==0: 
+				with open(f"{folder}{logfile}", "w"):
+					pass
+			self.logger(file=name, logfile=f"{folder}{logfile}")
+
+	def limiter(self, channels, dataset=None, xmax=None, xmin=None,ymax=None, ymin=None, nclusters=2, save=False, **kwargs):
+		print("[LIMITER] Running the limiter")
+		if not dataset: dataset = self.dataset
+		x = channels[0]
+		y = channels[1]
+
+		# For convenience; who gives a shit about overhead.
+		upper_limit = lambda name, _max: dataset[name].apply(lambda k: 1 if k <= _max else 0)
+		lower_limit = lambda name, _min: dataset[name].apply(lambda k: 1 if k >= _min else 0)
+
+		if xmax: dataset["mapping"] = upper_limit(x, xmax)
+		if ymax: dataset["mapping"] = upper_limit(y, ymax)
+
+		if xmin: dataset["mapping"] = lower_limit(x, xmin)
+		if ymin: dataset["mapping"] = lower_limit(y, ymin)
+
+		order = [i for i in range(nclusters)]
+		fg    = seaborn.FacetGrid(data=dataset, hue="mapping", hue_order=order, aspect=1.61)
+		fg.map(plt.scatter, x, y).add_legend()
+		if not save: plt.show()
+
+	def plot_map(self, x=None, y=None, dataset=None, xfunc=None, yfunc=None, transpose=False, save=False, threeD=False, nclusters=2, **kwargs):
+		print("[MAPPER] Running the map plotter")
+		if not dataset: dataset = self.dataset
+		order = [i for i in range(nclusters)]
+		fg    = seaborn.FacetGrid(data=dataset, hue="mapping", hue_order=order, aspect=1.61)
+		fg.map(plt.scatter, x, y).add_legend()
+		if not save: plt.show()
+
+	def logger(self, file, clusters=2, dataset=None, map="mapper", logfile="log.txt", state="a", folder=None, **kwargs):
+		if not dataset: dataset = self.dataset
+
+		data  = list(dataset["mapping"])
+		first = 0 
+		tot   = len(data)
+		for i in data:
+			if i == 1: first +=1
+
+		ratio = f"{(first/tot)*100}%"
+		to_write=f"**************\nSample: {file}\n debris/total: {ratio}\n"
+		with open(logfile, state) as file:
+			file.write(to_write)
 
 def _log(i):
 	if i > 0: return log(i)
 	else: return None
 
-
 if __name__ == '__main__':
 	use("Agg")
 	run = Analyze()
 	run.read()
-
 	# run.kmeans(channels=["FSC-A", "SSC-A"], logx=False, logy=True, transpose=False, nclusters=3)
 	# run.plot(x="FSC-A", y="SSC-A",  kind="scatter", transpose=False)
 	# run.plot(x="FSC-A", y="SSC-A", yfunc=_log, kind="scatter", transpose=False)
 	# freq = run.freq("FSC-A", scope=500)
 	# run.plot(x="FSC-A", y="SSC-A", yfunc=_log, kind="scatter", transpose=False, save=True)
+	# plt.show()
 	# plt.savefig("blah.png")
 	# print(freq[])
-
-
 	# run.plot(x=freq[0], y=run.dataset["FSC-A"], kind="scatter")
 	# run.saveplots(run.freq, column="FSC-A", scope=500, rdata=True, delimiter='\\')
-
 	# run.saveplots(run.plot, x="FSC-A", y="SSC-A", yfunc=_log, kind="scatter", transpose=False, save=True, description="FSC-A_ON_SSC-A")
-	# run.saveplots(run.kmeans, channels=["FSC-A", "SSC-A"], logx=False, logy=True, transpose=False, nclusters=3, description="kmeans")
-
-
-
-
-
+	# print(run.dataset["SSC-A"])
+	# run.saveplots(run.kmeans, channels=["FSC-A", "SSC-A"], logx=False, logy=False, transpose=True, nclusters=4, description="kmeanS2", limit_dataset=None)
+	# run.saveplots(run.plot_3d, x="FSC-A",  z="APC-A", y="FITC-A", yfunc=_log, kind="scatter", transpose=False, save=True, description="FITC")
+	# run.limiter(channels=["SSC-A", "FSC-A"], xmax=2000)
+	# run.plot(x="FSC-A", y="SSC-A", yfunc=_log, kind="scatter")
+	run.saveplots(run.limiter, channels=["FSC-A", "SSC-A"], xmax=25000, save=True, description="limite")
